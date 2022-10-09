57d5037b98a061145e385a2b02eb710a7ce97edd
chrisxyq <yuanqixu@trip.com>
2022-10-09 17:14:54 +0800

    scheduledExecutorService.scheduleAtFixedRate

---
+++ src/main/java/threadPool/InFlightRequests.java
@@ -0,0 +1,84 @@
+package threadPool;
+
+import org.junit.Test;
+import threadPool.dto.ResponseFuture;
+
+import java.io.Closeable;
+import java.util.Map;
+import java.util.concurrent.*;
+
+public class InFlightRequests implements Closeable {
+    private final static long                         TIMEOUT_SEC              = 10L;
+    /**
+     * 在实现异步网络传输的时候，一定要配套实现一个背压的机制，避免客户端请求速度过快，导致大量的请求失败。
+     * 在服务端处理不过来的时候限制一下客户端的请求速度。
+     * 这个信号量有 10 个许可，我们每次往 inFlightRequest 中加入一个 ResponseFuture 的时候，
+     * 需要先从信号量中获得一个许可，如果这时候没有许可了，就会阻塞当前这个线程，也就是发送请求的这个线程，
+     * 直到有人归还了许可，才能继续发送请求。我们每结束一个在途请求，就归还一个许可，
+     * 这样就可以保证在途请求的数量最多不超过 10 个请求，积压在服务端正在处理或者待处理的请求也不会超过 10 个。
+     */
+    private final        Semaphore                    semaphore                = new Semaphore(10);
+    /**
+     * 用于维护所有的在途请求，key为请求id，value为返回值
+     */
+    private final        Map<Integer, ResponseFuture> futureMap                = new ConcurrentHashMap<>();
+    private final        ScheduledExecutorService     scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private final        ScheduledFuture              scheduledFuture;
+
+    public InFlightRequests() {
+        /**
+         * 初始化时延时0s开始执行，本次执行结束后延迟1s开始下次执行
+         */
+        scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(this::removeTimeoutFutures, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public void put(ResponseFuture responseFuture) throws InterruptedException, TimeoutException {
+        if (semaphore.tryAcquire(TIMEOUT_SEC, TimeUnit.SECONDS)) {
+            futureMap.put(responseFuture.getRequestId(), responseFuture);
+        } else {
+            throw new TimeoutException();
+        }
+    }
+
+    /**
+     * 即使是我们对所有能捕获的异常都做了处理，也不能保证所有 ResponseFuture 都能正常或者异常结束，
+     * 比如说，编写对端程序的程序员写的代码有问题，收到了请求就是没给我们返回响应，
+     * 为了应对这种情况，还必须有一个兜底超时的机制来保证所有情况下 ResponseFuture 都能结束，
+     * 无论什么情况，只要超过了超时时间还没有收到响应，我们就认为这个 ResponseFuture 失败了，结束并删除它
+     */
+    private void removeTimeoutFutures() {
+        futureMap.entrySet().removeIf(entry -> {
+            if (System.nanoTime() - entry.getValue().getTimestamp() > (TIMEOUT_SEC - 5) * 1000000000L) {
+                System.out.println("semaphore.release():" + entry.getValue().getRequestId());
+                semaphore.release();
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
+    public ResponseFuture remove(int requestId) {
+        ResponseFuture future = futureMap.remove(requestId);
+        if (null != future) {
+            semaphore.release();
+        }
+        return future;
+    }
+
+    @Override
+    public void close() {
+        scheduledFuture.cancel(true);
+        scheduledExecutorService.shutdown();
+    }
+
+    @Test
+    public void test() throws InterruptedException, TimeoutException {
+        InFlightRequests inFlightRequests = new InFlightRequests();
+        for (int i = 0; i < 15; i++) {
+            System.out.println(i);
+            inFlightRequests.put(new ResponseFuture(i, new CompletableFuture<>()));
+        }
+        inFlightRequests.close();
+    }
+}
--- src/main/java/threadPool/ThreadPoolExceptionTest.java
+++ src/main/java/threadPool/ThreadPoolExceptionTest.java
@@ -13,21 +13,34 @@ import java.util.stream.IntStream;
  */
 public class ThreadPoolExceptionTest {
     /**
+     * 默认情况下创建的线程名字都类似pool-1-thread-2这样
+     */
+    private static ThreadPoolExecutor threadPoolExecutorDefault        = new ThreadPoolExecutor(1, 1,
+            60, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(200), Executors.defaultThreadFactory());
+    /**
+     * ThreadFactoryBuilder是guava提供的ThreadFactory生成器
+     */
+    private static ThreadPoolExecutor threadPoolExecutor               = new ThreadPoolExecutor(1, 1,
+            60, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(200),
+            new ThreadFactoryBuilder().setNameFormat("customThread %d").build());
+    /**
      * 如果要捕获那些没被业务代码捕获的异常，可以设置Thread类的uncaughtExceptionHandler属性。
      * 这时使用ThreadFactoryBuilder会比较方便，ThreadFactoryBuilder是guava提供的ThreadFactory生成器
+     * <p>
+     * 如果任务在执行的过程中出现运行时异常，会导致执行任务的线程终止；
+     * 最稳妥和简单的方案还是捕获所有异常并按需处理
      */
-    private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 60, TimeUnit.SECONDS,
-            new ArrayBlockingQueue<>(200), new ThreadFactoryBuilder().setNameFormat("customThread %d").build());
-    private static ThreadPoolExecutor threadPoolExecutorHandleUncaught = new ThreadPoolExecutor(1, 1, 60, TimeUnit.SECONDS,
+    private static ThreadPoolExecutor threadPoolExecutorHandleUncaught = new ThreadPoolExecutor(1,
+            1, 60, TimeUnit.SECONDS,
             new ArrayBlockingQueue<>(200),
             new ThreadFactoryBuilder()
                     .setNameFormat("customThread %d")
-                    .setUncaughtExceptionHandler((t, e) -> System.out.println("UncaughtExceptionHandler捕获到：" + t.getName() + "发生异常" + e.getMessage()))
+                    .setUncaughtExceptionHandler((t, e) ->
+                            System.out.println("UncaughtExceptionHandler捕获到：" + t.getName() + "发生异常" + e.getMessage()))
                     .build());
 
-    /**
-     * 可见每次执行的线程都不一样，之前的线程都没有复用。原因是因为出现了未捕获的异常
-     */
     @Test
     public void test() {
         IntStream.rangeClosed(1, 5).forEach(i -> {
@@ -36,15 +49,16 @@ public class ThreadPoolExceptionTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            /**
+             * 线程池执行execute方法，当出现了未捕获的异常，每次执行的线程都不一样，之前的线程都没有复用
+             */
             threadPoolExecutor.execute(() -> {
                 int j = 1 / 0;
             });
         });
     }
 
-    /**
-     * 当异常捕获了，线程就可以复用了
-     */
+
     @Test
     public void test1() {
         IntStream.rangeClosed(1, 5).forEach(i -> {
@@ -53,6 +67,10 @@ public class ThreadPoolExceptionTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            /**
+             * 线程池执行execute
+             * 当异常捕获了，线程就可以复用了
+             */
             threadPoolExecutor.execute(() -> {
                 try {
                     int j = 1 / 0;
@@ -62,10 +80,7 @@ public class ThreadPoolExceptionTest {
             });
         });
     }
-    /**
-     * 捕获到异常，但是线程依旧没有复用
-     * 通过submit提交线程，无法达到线程复用。
-     */
+
     @Test
     public void test2() {
         IntStream.rangeClosed(1, 5).forEach(i -> {
@@ -74,13 +89,17 @@ public class ThreadPoolExceptionTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
+            /**
+             * 捕获到异常，又将异常抛出，
+             * 因此线程依旧没有复用
+             */
             threadPoolExecutorHandleUncaught.execute(() -> {
                 System.out.println("线程" + Thread.currentThread().getName() + "执行");
                 int j = 1 / 0;
             });
         });
     }
+
     /**
      * 捕获到异常，线程成功复用
      * 通过submit提交线程可以屏蔽线程中产生的异常，达到线程复用。当get()执行结果时异常才会抛出。
@@ -94,7 +113,6 @@ public class ThreadPoolExceptionTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             Future<?> future = threadPoolExecutor.submit(() -> {
                 System.out.println("线程" + Thread.currentThread().getName() + "执行");
                 int j = 1 / 0;
